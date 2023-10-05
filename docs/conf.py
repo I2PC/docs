401a83a039568b38a229d404abb497893628065f
alberto <12760268+albertmena@users.noreply.github.com>
2023-10-05 08:06:07 +0200

    commit

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = u'Xmipp'
-copyright = u'2022, Xmipp team'
+copyright = u'2023, Xmipp team'
 author = u'Xmipp team'
 
 # The short X.Y version
