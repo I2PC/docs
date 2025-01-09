7a635e49a50dbc4d674f3fa63cf51360235744c4
alberto <alberto.garcia@cnb.csic.es>
2025-01-09 05:58:52 +0000

    .

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = u'Xmipp'
-copyright = u'2024, Xmipp team'
+copyright = u'2025, Xmipp team'
 author = u'Xmipp team'
 
 # The short X.Y version
